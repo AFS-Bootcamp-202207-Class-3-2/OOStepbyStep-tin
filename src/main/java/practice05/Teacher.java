09ce64f54e93f4f68f167eca652b7bc50cd16cb4
Tin Chen <chenti11@oocl.com>
2022-07-20 16:25:22 +0800

    feat: practice05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,27 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if(this.getKlass() == null){
+            return "My name is " + this.getName() + ". I am " + this.getAge() + " years old. I am a Teacher. I teach No Class.";
+        }else{
+            return "My name is " + this.getName() + ". I am " + this.getAge() + " years old. I am a Teacher. I teach Class " + this.getKlass() + ".";
+        }
+    }
 }
