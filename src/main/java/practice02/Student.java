70e2c1802e9f76677aa7cdd3174911c694a02f67
Tin Chen <chenti11@oocl.com>
2022-07-20 16:14:16 +0800

    feat: practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,27 @@
 package practice02;
 
-public class Student {
+import practice06.Klass;
+
+public class Student extends Person{
+
+
+    private Integer klass;
+
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        return "I am a Student. I am at Class " + this.getKlass() + ".";
+    }
 }
--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,24 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+
+    private Integer klass;
+
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        return "I am a Student. I am at Class " + this.getKlass() + ".";
+    }
 }
