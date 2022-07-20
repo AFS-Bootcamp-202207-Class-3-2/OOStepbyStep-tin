55f2184db171471d0fe948fa92a0a0a6dd8a9b7f
Tin Chen <chenti11@oocl.com>
2022-07-20 16:18:55 +0800

    feat: practice04

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,25 @@
 package practice04;
 
-public class Student {
+import practice03.Person;
+
+public class Student extends Person {
+    private Integer klass;
+
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        return "My name is " + this.getName() + ". I am " + this.getAge() + " years old. I am a Student. I am at Class " + this.getKlass() + ".";
+    }
 }
